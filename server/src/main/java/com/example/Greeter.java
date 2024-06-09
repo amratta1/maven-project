2b0e860d96d3075b9a6b8edd29400e0e67032cec
amr atta <you@example.com>
2024-06-09 08:14:08 +0300

    update

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,13 @@ public class Greeter {
   public Greeter() {
 
   }
+
   /**
    * @param some person greetings  
    * @return  greetings setring 
-   * /
+   */
 
-  public final String greet( final String someone) {
+   public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
